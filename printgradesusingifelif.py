974eb0b6e5f2c4f5046da84ae61cbb1ec0e994fe
Mounika Pawar <mounikapawar542@gmail.com>
2025-09-03 19:11:22 +0530

    printgradesusingifelif.py

---
+++ printgradesusingifelif.py
@@ -0,0 +1,11 @@
+marks=int(input())
+if(marks>=90):
+    print("Grade A")
+elif(marks>=70 and marks<90):
+    print("Grade B")
+elif(marks>=50 and marks<70):
+    print("Grade C")
+elif(marks>=35 and marks<50):
+    print("Grade D")
+else:
+    print("Fail")
